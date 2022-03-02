33fe5786a84d62250fdbf3f17c41b58d14aec1c5
lv2web <all2web@mail.ru>
2022-03-02 16:48:19 +0300

    added block "show"

--- js/script.js
+++ js/script.js
@@ -22,11 +22,21 @@ function ibg(){
 /* ======================================================================================== */
 
 /*===< slider >=================================================== */
-$(document).ready(function(){
+/* $(document).ready(function(){ */
    $('.testimonials__block').slick({
-      slideToShow:1,
+      //infinite: true,
       dots:true,
-   });
+      arrows:false,
+      accessibility:false,
+      slidesToShow: 1,
+      autoplaySpeed: 3000,
+      //adaptiveHeight:true,
+      nextArrow:'<button type="button" class="slick-next"></button>',
+      prevArrow:'<button type="button" class="slick-prev"></button>',
+      responsive: [{
+         breakpoint: 768,
+         settings: {}
+      }]
 });
 
 /*===< for IBG technology pure JS >=================================================== */
